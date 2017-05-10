46688a24cee8ff3b88990dbf80e5949a98ed8be5
buyan <buyan206@163.com>
2017-05-10 17:30:45 +0800

    add assets and set the background

--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -11,43 +11,30 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    
+    var scene : GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        // configure the view.
+        print("confiture the view")
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // create and configure the scene
+        print("create and configure the scene")
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFit
+        
+        // present the scene
+        print("present the scene")
+        skView.presentScene(scene)
         }
-    }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
+ 
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+   
 
     override var prefersStatusBarHidden: Bool {
         return true
